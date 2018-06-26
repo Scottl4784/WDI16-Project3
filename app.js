d5aed88fa33b0f69c6ca287c6691fb11e41bae11
Scottl4784 <Scottl4784@gmail.com>
2018-06-26 11:42:54 -0400

    brought in dependencies into express

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.connect(process.env.MONGODB_URI)
 
-var app = express();
+const db = mongoose.connection
+db.on('error', err => {
+    console.log(err)
+})
+db.on('open', () => {
+    console.log('Connected to MongoDB')
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 3001
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const moviesRouter = require('./routes/movies')
+const commentsRouter = require('.routes/comments')
 
-module.exports = app;
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+module.exports = app
