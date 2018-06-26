c5ddc8052412661bb77a6e7d5617d3b1364e5807
Scottl4784 <Scottl4784@gmail.com>
2018-06-26 14:34:51 -0400

    added routes for user delete and create

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {User} = require('../db/schema')
 
-/* GET users listing. */
+// Get list of users
 router.get('/', (req, res) => {
   User.find().then(users => {
     res.send({users})
@@ -14,7 +14,41 @@ router.get('/', (req, res) => {
 
 // Show single user
 router.get('/:userId', (req, res) => {
-  
+  User.findById(req.params.userId)
+    .then((user) => {
+      res.send(user)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+// Create a new user
+router.post('/', (req, res) => {
+  const newUser = new User (req.body)
+  newUser.save()
+  .then((user) => {
+    res.send(user)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+// Update user
+
+// Delete user
+router.delete('/:userId', (req, res) => {
+  User.findByIdAndRemove(req.params.userId)
+  .then(() => {
+    User.find()
+    .then((users) => {
+      res.send({users})
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 })
 
 module.exports = router
