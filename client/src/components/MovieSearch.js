165918efe7a5169b180ddc7912228580f3b273b1
Scottl4784 <Scottl4784@gmail.com>
2018-07-02 09:24:29 -0400

    fixed movie search not resetting

--- client/src/components/MovieSearch.js
+++ client/src/components/MovieSearch.js
@@ -68,37 +68,37 @@ class MovieSearch extends Component {
         const userId = this.props.match.params.userId
         axios.post(`/api/users/${userId}/movies`, this.state.searchResults[i]).then((res) => {
             this.props.newMovie(res.data.user.movies)
-            console.log(res)
             this.resetState()
         })
-        .catch((err) => {
-            console.log(err)
-        })
+            .catch((err) => {
+                console.log(err)
+            })
     }
     handleSearch = () => {
         axios.get(`https://www.omdbapi.com/?t=${this.state.search}&apikey=66f47cb`)
             .then((res) => {
-            const newSearchResults = [...this.state.searchResults]
-            newSearchResults.push(res.data)
-            this.setState({ searchResults: newSearchResults })
+                const newSearchResults = [...this.state.searchResults]
+                newSearchResults.push(res.data)
+                this.setState({ searchResults: newSearchResults })
             })
     }
     resetState = () => {
-        const resetState = {...this.state}
+        const resetState = { ...this.state }
         resetState.search = []
         resetState.searchResults = []
         this.setState(resetState)
     }
     toggleSearch = () => {
         const searchForMovie = !this.state.searchForMovie
-        this.setState({searchForMovie})
+        this.setState({ searchForMovie })
     }
     handleKeyPress = (event) => {
-        if(event.key === 'Enter'){
-          this.handleSearch()
+        if (event.key === 'Enter') {
+            this.handleSearch()
+            this.resetState()
         }
-      }
-    
+    }
+
 
 
     render() {
@@ -114,14 +114,14 @@ class MovieSearch extends Component {
                         onChange={this.handleChange}
                         onKeyPress={this.handleKeyPress}
                     />
-                    </SearchBar>
-                    {this.state.searchResults.map((result, i) => {
-                        return (
-                            <SearchResults key={i}>                                
-                                <button onClick={() => this.handleSubmit(i)}><img src={result.Poster} alt=""/></button>
-                            </SearchResults>
-                        )
-                    })}
+                </SearchBar>
+                {this.state.searchResults.map((result, i) => {
+                    return (
+                        <SearchResults key={i}>
+                            <button onClick={() => this.handleSubmit(i)}><img src={result.Poster} alt="" /></button>
+                        </SearchResults>
+                    )
+                })}
 
             </Container>
         );
